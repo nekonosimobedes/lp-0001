2dc0cc915280950d082280faf4d26a73089d526e
nekonosimobedes <nekonosimobedesu@gmail.com>
2026-03-03 15:49:00 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//スムーススクロール
+$('a[href^="#"]').on('click', function () {
+  var speed = 300;
+  var href = $(this).attr("href");
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var position = target.offset().top;
+  $("html, body").animate({
+    scrollTop: position
+  }, speed, "swing");
+  return false;
+});
+
